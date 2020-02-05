b8176068fecb1c6ce74842fb1beef241543a433e
Soare Alina <soaree.alina@yahoo.ro>
2020-02-05 15:33:28 +0200

    another commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const helloWorld = 'Welcome!';
+  const user = {
+    first_name: 'Popescu',
+    last_name: 'Pop'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{helloWorld}</h1>
+      <p>{user.first_name}</p><br />
+      <p>{user["last_name"]}</p>
     </div>
   );
 }
