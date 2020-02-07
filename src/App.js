41ce6e4b2f662a487bfa24c4b4639c6fd0cff6b4
Soare Alina <soaree.alina@yahoo.ro>
2020-02-07 16:48:05 +0200

    till page 61

--- src/App.js
+++ src/App.js
@@ -2,19 +2,87 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const helloWorld = 'Welcome!';
-  const user = {
-    first_name: 'Popescu',
-    last_name: 'Pop'
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
   }
-  return (
-    <div className="App">
-      <h1>{helloWorld}</h1>
-      <p>{user.first_name}</p><br />
-      <p>{user["last_name"]}</p>
-    </div>
-  );
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list,
+      searchTerm: ''
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onDismiss(id) {
+    const filteredList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({
+      list: filteredList
+    });
+  }
+
+  onSearchChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+
+  }
+
+
+  render(){
+    const {searchTerm, list} = this.state;
+    return (
+      <div className="App">
+      <form>
+        <input
+          type='text' 
+          onChange={this.onSearchChange}
+        />
+      </form>
+      {list.filter(isSearched(searchTerm)).map(item =>
+           <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button 
+                onClick={() => this.onDismiss(item.objectID)}
+                type='button'>
+                  Dismiss
+              </button>
+            </span>
+          </div>
+      )}
+      </div>
+    );
+  }
 }
 
 export default App;
